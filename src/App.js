b7799c8795fed4e843e1a618f5a9b48353f20f00
bassem eloraby <BASEM12683@GMAIL.COM>
2023-05-06 02:55:55 +0300

    add - css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addUser } from './features/Users';
 
 function App() {
+  const dispatch = useDispatch();
   const userList = useSelector((state) => state.users.value);
+
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
 
   return (
     <div className="App">
       <div className="addUser">
-        <input type="text" placeholder="Name..." />
-        <input type="text" placeholder="Username..." />
-        <button>Add User</button>
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            dispatch(
+              addUser({
+                id: userList[userList.length - 1].id + 1,
+                name,
+                username,
+              })
+            );
+          }}
+        >
+          Add User
+        </button>
       </div>
       <div className="displayUsers">
         {userList.map((user) => {
