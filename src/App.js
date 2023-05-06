6cd768f509be159c1fd424833b003118eadf6577
bassem eloraby <BASEM12683@GMAIL.COM>
2023-05-06 13:21:25 +0300

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 // import Users from './pages/Users';
 
 import Companies from './pages/Companies';
@@ -6,6 +8,9 @@ import Companies from './pages/Companies';
 function App() {
   return (
     <div className="App">
+    <Button as="a" variant="primary">
+    Button as link
+  </Button>
       {/*<Users />*/}
       <Companies />
     </div>
