32fe5491c8d4a6ff0f1a9e8f630b452e7f4e8c5d
bassem eloraby <BASEM12683@GMAIL.COM>
2023-05-06 15:35:09 +0300

    company table

--- src/pages/Companies.js
+++ src/pages/Companies.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getComps } from '../features/companiesReducer';
+import Table from 'react-bootstrap/Table';
 
 function Companies() {
   const { comps, loading } = useSelector((state) => state.company);
@@ -19,13 +20,40 @@ function Companies() {
       {' '}
       {/*-------start company header----------*/}
       <div className="">
-      <h2 className="text-center">Company </h2>
-      <div className="underline"></div>
-    </div>
-    {/*-------end company header----------*/}
-      {comps.map((item) => (
-        <h2 key={item._id}>{item.companyName}</h2>
-      ))}
+        <h2 className="text-center">Company </h2>
+        <div className="underline"></div>
+      </div>
+      {/*-------end company header----------*/}
+      {/*-------start company show-------*/}
+      <section className="col">
+        <Table striped bordered hover>
+          <thead>
+            <tr style={{ backgroundColor: 'orange' }}>
+              <th>Company</th>
+              <th>Website</th>
+            </tr>
+          </thead>
+          <tbody>
+            {comps.map((comp) => (
+              <tr key={comp._id}>
+                <td>{comp.companyName}</td>
+                <td>
+                  <a
+                    href={comp.website}
+                    rel="noreferrer"
+                    target="_blank"
+                    style={{ textDecoration: 'underline' }}
+                  >
+                    {comp.companyName}
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </section>
+      {/*-------end company show-------*/}
+ 
     </div>
   );
 }
