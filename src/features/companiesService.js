e5ab17359fe0d048f9bd9f6f134f21cdc6651540
bassem eloraby <BASEM12683@GMAIL.COM>
2023-05-06 14:28:49 +0300

    axios

--- src/features/companiesService.js
+++ src/features/companiesService.js
@@ -1,12 +1,15 @@
+import axios from 'axios';
+
+const API_URL = 'https://sore-lime-goat-tam.cyclic.app/api/companies';
+
 const getCompanies = async () => {
-    return fetch('https://sore-lime-goat-tam.cyclic.app/api/companies').then(
-      (res) => res.json()
-    );
-  }
+  const response = await axios.get(API_URL);
 
+  return response.data;
+};
 
-  const companiesService = {
-    getCompanies,
-  };
-  
-  export default companiesService;
+const companiesService = {
+  getCompanies,
+};
+
+export default companiesService;
